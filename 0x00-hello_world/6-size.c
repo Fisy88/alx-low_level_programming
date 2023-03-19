2d6af5962474e630c4c2740b694e4c318cc34e97
Fisy88 <fisy87@gmail.com>
2023-03-19 16:14:55 -0700

    6-size.c file correcting

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,18 +6,11 @@
  * Return: Always 0.
  */
 
-int main(void)
-{
-	printf("Size of char: %d byte(s)\n", sizeof(char));
-	printf("Size of int: %d byte(s)\n", sizeof(int));
-	printf("Size of short int: %d byte(s)\n", sizeof(short int));
-	printf("Size of long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of float: %d byte(s)\n", sizeof(float));
-	printf("Size of double: %d byte(s)\n", sizeof(double));
-	printf("Size of long double: %d byte(s)\n", sizeof(long double));
-	printf("Size of char*: %d byte(s)\n", sizeof(char *));
-	printf("Size of int*: %d byte(s)\n", sizeof(int *));
-	printf("Size of void*: %d byte(s)\n", sizeof(void *));
+int main(void){
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
