c13234268636af2a7fe51c65c881d97cd04dd81c
Fisy88 <fisy87@gmail.com>
2023-03-19 10:31:14 -0700

    fourth c file4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
+
 /** 
- * The main() function is the entry point of the program.
- * The puts() function is used to print the desired string followed by a new line.
- * The return 0 statement is used to indicate that the program completed successfully with exit code 0
+ * main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
-int main(void)
+ int main(void)
 
 {
-       	puts("Programming is like building a multilingual puzzle");
-       	return 0; 
+       	puts("\"Programming is like building a multilingual puzzle");
+       	return (0); 
 
 }
