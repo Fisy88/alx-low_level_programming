ce053cddd2595d66c7d85e09fcea22e5cc6c1d6e
Fisy88 <fisy87@gmail.com>
2023-04-02 12:45:34 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,11 +8,11 @@
 
 int _isalph(int c)
 {
-if ((c >='a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return 1;
-}
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+
 else
-{
+
 return (0);
-}
+
 }
